e79090bc8c80dfaa836b97713ef3c83f90c5e400
Ömer Faruk Yığın <42890397+omert11@users.noreply.github.com>
2025-05-07 23:48:32 +0300

    Refactor calculator logic to use an enum for operators and streamline operation handling. Introduced helper functions for operator identification and operation execution, improving code readability and maintainability.

--- src/calculator_v2.rs
+++ src/calculator_v2.rs
@@ -1,5 +1,13 @@
 use crate::ui;
 
+enum Operator {
+    Eqq,
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
 fn _try_parse_float(value: &str) -> f64 {
     match value.parse::<f64>() {
         Ok(v) => v,
@@ -7,52 +15,66 @@ fn _try_parse_float(value: &str) -> f64 {
     }
 }
 
+fn _do_operation(operator: Operator, current: f64, value: f64) -> f64 {
+    match operator {
+        Operator::Eqq => value,
+        Operator::Add => current + value,
+        Operator::Sub => current - value,
+        Operator::Mul => current * value,
+        Operator::Div => current / value,
+    }
+}
+
+fn _is_operator(value: char) -> bool {
+    value == '+' || value == '-' || value == '*' || value == '/'
+}
+
+fn _get_operator(value: char) -> Operator {
+    match value {
+        '+' => Operator::Add,
+        '-' => Operator::Sub,
+        '*' => Operator::Mul,
+        '/' => Operator::Div,
+        _ => Operator::Eqq,
+    }
+}
 pub fn calculator() {
     println!("Calculator v2");
     println!("enter 'exit' to quit");
     println!("############");
 
+    let mut operator: Operator = Operator::Eqq;
     let mut result: f64 = 0.0;
-    let mut last_value: String = String::new();
-    loop {
-        let value: String = ui::get_input("");
 
-        if value == "exit" {
+    loop {
+        let input: String = ui::get_input("");
+
+        if input == "exit" {
             break;
         }
 
-        let mut prev_value: String = String::new();
+        let mut value: String = String::new();
 
-        value.chars().for_each(|x| {
+        for x in input.chars() {
             if x == ' ' {
-                return;
+                continue;
             }
 
-            if x == '+' || x == '-' || x == '*' || x == '/' {
-                if last_value.is_empty() {
-                    last_value.push_str(&prev_value);
-                    prev_value.clear();
-                    return;
-                }
-                
-                let v = _try_parse_float(&last_value);
-                match x {
-                    '+' => result += v,
-                    '-' => result -= v,
-                    '*' => result *= v,
-                    '/' => result /= v,
-                    _ => result = v,
-                }
-                last_value.clear();
-            } else {
-                prev_value.push(x);
+            if _is_operator(x) {
+                let parsed_value = _try_parse_float(&value);
+                result = _do_operation(operator, result, parsed_value);
+                operator = _get_operator(x);
+                value.clear();
+                continue;
             }
-        });
 
-        if !last_value.is_empty() {
-            result = _try_parse_float(&last_value);
+            value.push(x);
         }
-
+        if !value.is_empty() {
+            let parsed_value = _try_parse_float(&value);
+            result = _do_operation(operator, result, parsed_value);
+        }
+        operator = Operator::Eqq;
         println!("Result: {}", result)
     }
 }
